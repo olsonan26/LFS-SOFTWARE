6dd49750e7fbcce8ad127a191e076ef00413bfdc
olsonan26 <youwillalertme@gmail.com>
2026-09-20 05:30:24 -0600

    Install reliable print overrides and white print canvas

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './print.css';
 import './components/PersonDossierPrint.css';
 import './components/PersonDossierPrintFill.css';
 import './components/PersonDossierSelections.css';
 import './components/StudentTimelineControls.css';
+import { installPrintOverrides } from './utils/installPrintOverrides';
+
+installPrintOverrides();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
